93edf663b120b3eabd5874c2997df70afe3c99f2
Josh  Stewart <jmstewart00@gmail.com>
2020-12-31 09:45:26 -0500

    Add commerce, feeds, spirits stuff, more

--- web/modules/custom/custom_layout/src/Plugin/Layout/LayoutBase.php
+++ web/modules/custom/custom_layout/src/Plugin/Layout/LayoutBase.php
@@ -30,14 +30,6 @@ abstract class LayoutBase extends LayoutDefault implements PluginFormInterface {
       $build['#attributes']['class'][] = 'l-hero-row';
     }
 
-    $class = $this->configuration['class'];
-    if ($class) {
-      $build['#attributes']['class'] = array_merge(
-        explode(' ', $this->configuration['class']),
-        $build['#attributes']['class']
-      );
-    }
-
     return $build;
   }
 
@@ -59,12 +51,6 @@ abstract class LayoutBase extends LayoutDefault implements PluginFormInterface {
 
     $pluginId = $this->getPluginId();
 
-    $form['layout'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Layout'),
-      '#open' => TRUE,
-    ];
-
     $form['background'] = [
       '#type' => 'details',
       '#title' => $this->t('Background'),
@@ -75,6 +61,23 @@ abstract class LayoutBase extends LayoutDefault implements PluginFormInterface {
       '#type' => 'custom_layout_background_color_radios',
       '#plugin_id' => $this->getPluginDefinition()->id(),
       '#default_value' => $this->configuration['background_color'],
+    ];
+
+    $form['extra'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Extra'),
+      '#open' => TRUE,
+    ];
+
+    $form['extra']['hero'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Hero Mode'),
+      'checkbox' => [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Enable hero mode'),
+        '#description' => $this->t('Enabling hero mode will remove the left and right space of the row. This will make the row the width of the browser window.'),
+        '#default_value' => $this->configuration['hero'],
+      ],
     ];
 
     $form['#attached']['library'][] = 'custom_layout/layout_builder';
@@ -95,6 +98,7 @@ abstract class LayoutBase extends LayoutDefault implements PluginFormInterface {
     $values = $form_state->getValues();
 
     $this->configuration['background_color'] = $values['background']['background_color'];
+    $this->configuration['hero'] = (bool) $values['extra']['hero']['checkbox'];
 
   }
 
@@ -111,6 +115,16 @@ abstract class LayoutBase extends LayoutDefault implements PluginFormInterface {
     else {
       return FALSE;
     }
+  }
+
+  /**
+   * Determine if this layout has extra settings.
+   *
+   * @return bool
+   *   If this layout has extra settings.
+   */
+  protected function hasExtraSettings(): bool {
+    return $this->configuration['class'] || $this->configuration['hero'];
   }
 
 }
