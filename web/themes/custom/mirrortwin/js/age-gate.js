b83b6622cd6abe7a716fa0d3828e37a5f6a84be8
JMStewart00 <jmstewart00@gmail.com>
2021-11-21 01:47:52 -0500

    Update login image

--- web/themes/custom/mirrortwin/js/age-gate.js
+++ web/themes/custom/mirrortwin/js/age-gate.js
@@ -10,33 +10,36 @@
    */
   Drupal.behaviors.ageGate = {
     attach: function (context, settings) {
-      if (!$.cookie("mirrorTwinAgeGate") || $.cookie("mirrorTwinAgeGate") === 'false') {
-        $("#ageGate").modal({
-          escapeClose: false,
-          clickClose: false,
-          showClose: false,
-          fadeDuration: 500,
-          fadeDelay: 0.80
+      $(body, context).once('age-gate').each(function () {
+
+        if (!$.cookie("mirrorTwinAgeGate") || $.cookie("mirrorTwinAgeGate") === 'false') {
+          $("#ageGate").modal({
+            escapeClose: false,
+            clickClose: false,
+            showClose: false,
+            fadeDuration: 500,
+            fadeDelay: 0.80
+          });
+        }
+
+        if (!$.cookie("mirrorTwinAgeGate")) {
+          $.cookie("mirrorTwinAgeGate", false, { path: '/' });
+        }
+
+        const yesBtn = $('#ageGateYes');
+
+        // Set an expiration date for 1 day from now, arbitrary amt of time.
+        const expiryDate = new Date();
+        expiryDate.setMonth(expiryDate.getDay() + 1);
+        // On the form submit button click, set a cookie.
+        yesBtn.on("click", (e) => {
+          e.preventDefault();
+          $.cookie("mirrorTwinAgeGate", true, { expires: 1 });
+          $.modal.close();
         });
-      }
 
-      if (!$.cookie("mirrorTwinAgeGate")) {
-        $.cookie("mirrorTwinAgeGate", false, { path: '/' });
-      }
-
-      const yesBtn = $('#ageGateYes');
-
-      // Set an expiration date for 1 day from now, arbitrary amt of time.
-      const expiryDate = new Date();
-      expiryDate.setMonth(expiryDate.getDay() + 1);
-      // On the form submit button click, set a cookie.
-      yesBtn.on("click", (e) => {
-        e.preventDefault();
-        $.cookie("mirrorTwinAgeGate", true, { expires: 1 });
-        $.modal.close();
       });
-
-    }
+    },
   };
 
 })(jQuery, Drupal);
