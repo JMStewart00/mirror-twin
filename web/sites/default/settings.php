cd572a3d8077706134519a60fc2a8dba6df585c8
JMStewart00 <jmstewart00@gmail.com>
2022-12-26 11:36:11 -0500

    Set up actions, ddev

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -90,3 +90,9 @@ if (isset($_ENV['AH_SITE_GROUP'], $_ENV['AH_SITE_ENVIRONMENT'])) {
 }
 
 $settings['update_free_access'] = TRUE;
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
